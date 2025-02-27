76cf2224c53ea5cb70c28c8752d77b2014eadfeb
Joan Egea <joanegea300@gmail.com>
2025-02-27 16:49:10 +0100

    registered component into quick action and sidebar region

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,16 +1,29 @@
-import { PrimariaApi, PrimariaMenuItem, shellRegions } from "@uxland/primary-shell";
+import { PrimariaApi, PrimariaNavItem, shellRegions } from "@uxland/primary-shell";
 import { MyElement } from "./my-element";
+import { QuickActionButton } from "./components/quick-action-button/quick-action-button";
 
 export const initialize = (api: PrimariaApi) => {
   console.log(`Plugin ${api.pluginInfo.pluginId} initialized`);
   api.regionManager.registerMainView({
     id: "plugin-main-view",
     factory: () =>  Promise.resolve(new MyElement()) ,
-  }, "Lit plugin");
+  },);
   
   api.regionManager.registerView(shellRegions.navigationMenu,{
+    id: "plugin-sidebar",
+    factory: () => {
+      const menuItem = new PrimariaNavItem({
+        icon: "add_box",
+        label: "Plugin Sidebar",
+        callbackFn: () => {
+        },
+      });
+      return Promise.resolve(menuItem);
+    },
+  });
+  api.regionManager.registerView(shellRegions.quickActions,{
     id: "plugin-quick-action",
-    factory: () => Promise.resolve(new PrimariaMenuItem("add_circle_outline", "Lit plugin", () => {
+    factory: () => Promise.resolve(new QuickActionButton("Lit plugin", () => {
       api.regionManager.activateMainView("plugin-main-view")
     })),
   });
@@ -19,6 +32,7 @@ export const initialize = (api: PrimariaApi) => {
 export const dispose = (api: PrimariaApi) => {
   console.log(`Plugin ${api.pluginInfo.pluginId} disposed`);
   api.regionManager.removeView(shellRegions.main, "plugin-main-view");
-  api.regionManager.removeView(shellRegions.navigationMenu, "plugin-quick-action");
+  api.regionManager.removeView(shellRegions.navigationMenu, "plugin-sidebar");
+  api.regionManager.removeView(shellRegions.quickActions, "plugin-quick-action");
   return Promise.resolve();
 }
