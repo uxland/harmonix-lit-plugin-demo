bd149828d7b870268a5dc5f5c04b6a8884a2ad2e
Joan Egea <joanegea300@gmail.com>
2025-10-28 17:19:38 +0100

    bump shell version and clinical monitoring added

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,5 +1,10 @@
 import { PluginDefinition, Plugin } from "@uxland/primary-shell";
 
 const importer: () => Promise<Plugin> = () => import("./plugin") as any;
+const clinicalMonitoringImporter: () => Promise<Plugin> = () => import("./clinical-monitoring/src/plugin") as any;
 
-export const plugins: PluginDefinition[] = [{ pluginId: "lit-plugin", importer: importer }];
+
+export const plugins: PluginDefinition[] = [
+  { pluginId: "lit-plugin", importer: importer },
+  { pluginId: "clinical-monitoring", importer: clinicalMonitoringImporter }
+];
