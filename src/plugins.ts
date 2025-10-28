91e1572d9938e459e9b4d432e636eac83e58b922
Joan Egea <joanegea300@gmail.com>
2025-10-28 17:31:47 +0100

    added admin clinical monitoring

--- src/plugins.ts
+++ src/plugins.ts
@@ -2,9 +2,10 @@ import { PluginDefinition, Plugin } from "@uxland/primary-shell";
 
 const importer: () => Promise<Plugin> = () => import("./plugin") as any;
 const clinicalMonitoringImporter: () => Promise<Plugin> = () => import("./clinical-monitoring/src/plugin") as any;
-
+const adminClinicalMonitoringImporter: () => Promise<Plugin> = () => import("./admin-clinical-monitoring/src/plugin") as any;
 
 export const plugins: PluginDefinition[] = [
   { pluginId: "lit-plugin", importer: importer },
-  { pluginId: "clinical-monitoring", importer: clinicalMonitoringImporter }
+  { pluginId: "clinical-monitoring", importer: clinicalMonitoringImporter },
+  /* { pluginId: "admin-clinical-monitoring", importer: adminClinicalMonitoringImporter }, */
 ];
