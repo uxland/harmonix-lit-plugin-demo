18b160483027840930419a697b6836440496d407
Joan Egea <joanegea300@gmail.com>
2025-11-05 16:03:10 +0100

    added bundled plugins

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,8 +1,10 @@
 import { PluginDefinition, Plugin } from "@uxland/primary-shell";
 
 const importer: () => Promise<Plugin> = () => import("./plugin") as any;
-const clinicalMonitoringImporter: () => Promise<Plugin> = () => import("./clinical-monitoring/src/plugin") as any;
-const adminClinicalMonitoringImporter: () => Promise<Plugin> = () => import("./admin-clinical-monitoring/src/plugin") as any;
+// @ts-ignore
+const clinicalMonitoringImporter: () => Promise<Plugin> = () => import("./clinical-monitoring.js") as any;
+// @ts-ignore
+const adminClinicalMonitoringImporter: () => Promise<Plugin> = () => import("./admin-clinical-monitoring.js") as any;
 
 export const plugins: PluginDefinition[] = [
   { pluginId: "lit-plugin", importer: importer },
