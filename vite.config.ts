e11fbb74f8443ed8a41c250f969de229b6f550d1
Xiaolin Jin Lin <dev@xiaojl.dev>
2026-07-30 17:59:04 +0200

    dev: rename vite config to .ts, type it and normalise formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
+import { type ConfigEnv, defineConfig, loadEnv } from "vite";
 import pkg from "./package.json";
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  
 
+export default ({ mode }: ConfigEnv) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     define: {
-      'process.env': {}, // Define process.env para evitar el error
+      "process.env": {}, // Define process.env para evitar el error
     },
     build: {
       lib: {
@@ -15,20 +14,17 @@ export default ({ mode }) => {
         // the proper extensions will be added
         fileName: "index",
         name: pkg.name,
-        
       },
-      
       rollupOptions: {
-        external: ["lit",   "@uxland/primary-shell", "@uxland/regions"],
+        external: ["lit", "@uxland/primary-shell"],
         output: {
           globals: {
             lit: "lit",
             "@uxland/primary-shell": "@uxland/primary-shell",
-            "@uxland/regions": "@uxland/regions",
           },
           inlineDynamicImports: true, // Incluye todas las importaciones dinámicas en un solo archivo
           manualChunks: undefined, // Desactiva la fragmentación para generar un solo archivo
-        }
+        },
       },
       minify: true,
       sourcemap: true,
